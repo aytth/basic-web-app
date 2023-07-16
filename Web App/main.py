6665a49def67de590becb0ad2b92b8c51d36b89a
aytth <123320778+aytth@users.noreply.github.com>
2023-07-16 13:14:13 +0400

    Updated main.py
    removed an unnecessary comment

--- Web App/main.py
+++ Web App/main.py
@@ -1,7 +1,6 @@
 # https://www.youtube.com/watch?v=dam0GPOAvVI&ab_channel=TechWithTim ----> 19:21 ---> 7/12/2023 (MM/DD)
 # 36:27, now 43:30, 52:35, 1:33:48
 # https://github.com/techwithtim/Flask-Web-App-Tutorial/blob/main/website/templates/base.html
-# vox: 46dh (standard), 
 
 from website import create_app
 
