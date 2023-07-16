1b3887bb6e8c3476627f34361991e274a7f127fd
aytth <123320778+aytth@users.noreply.github.com>
2023-07-16 13:14:38 +0400

    Update main.py
    cleaned the code

--- Web App/main.py
+++ Web App/main.py
@@ -1,7 +1,3 @@
-# https://www.youtube.com/watch?v=dam0GPOAvVI&ab_channel=TechWithTim ----> 19:21 ---> 7/12/2023 (MM/DD)
-# 36:27, now 43:30, 52:35, 1:33:48
-# https://github.com/techwithtim/Flask-Web-App-Tutorial/blob/main/website/templates/base.html
-
 from website import create_app
 
 app = create_app()
